515548c7504a07634cc301401fbdbe2a507710be
Liridona Isufi <isufi.liridona.dev@gmail.com>
2021-06-03 15:51:39 +0200

    last modifs

--- ex04/bissextile.py
+++ ex04/bissextile.py
@@ -1,4 +1,4 @@
-year = int(input("Entrez l année a verifier:\n"))
+year = int(input("Entrez l'année a verifier:\n"))
 if(year%4==0 and year%100!=0 or year%400==0):
     print("L'année est une année bissextile!")
 else:
