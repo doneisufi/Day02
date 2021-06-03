50408a73a521993671e627051befc3b8525eebb6
Liridona Isufi <isufi.liridona.dev@gmail.com>
2021-06-03 10:15:59 +0200

    Day02 Exercice 00)

---
+++ ex00/hitmachine2000.py
@@ -0,0 +1,16 @@
+
+textData = """Moi je m'appelle 'Lolita'
+Lo ou bien Lola, du pareil au même
+Moi je m'appelle 'Lolita'
+Quand je rêve aux loups, c'est Lola qui saigne
+[...]
+C'est pas ma faute
+Et quand je donne ma langue qux chats je vois les autres
+Tout prêts à se jeter sur moi, c'est pas ma faute à moi
+Si j'entends tout autour de moi
+L.O.L.I.T.A, moi 'Lolita'"""
+
+print(textData)
+
+
+
